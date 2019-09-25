8584096ec3722716f4898770d1a6aca579c4642d
Lucas Xavier <lucasxg07@hotmail.com>
2019-09-24 21:09:08 -0300

    Desafio de IMC,Tabuada e Impares

---
+++ desafi3.php
@@ -0,0 +1,20 @@
+
+<?php 
+		$peso = 10;
+		$altura = 1.70;
+		
+        $imc = $peso / $altura ** 2;
+        
+		
+        echo "Seu IMC é de $imc. voce está com o IMC ";
+        if ($imc <18.5){
+            echo "abaixo";
+         } elseif ($imc <25) {
+             echo "dentro";
+         } else {
+             echo "acima";
+         }
+
+        
+        
+		?>
